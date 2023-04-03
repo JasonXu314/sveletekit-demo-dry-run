ca073f0cd7d3aa7c787b59b4febf2f1854639395
Jason Xu <Xinwen31415@gmail.com>
2023-04-03 11:40:27 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Button from '@smui/button';
+	import Textfield from '@smui/textfield';
+	import Todo from './Todo.svelte';
+
+	let todoText: string = '';
+	let todos: { text: string; completed: boolean }[] = [];
+</script>
+
+<Textfield type="text" label="Todo Text" bind:value={todoText} />
+<Button
+	on:click={() => {
+		todos = [...todos, { text: todoText, completed: false }];
+		todoText = '';
+	}}
+	variant="raised"
+>
+	Add Todo
+</Button>
+
+{#each todos as todo}
+	<Todo bind:todo on:delete={() => (todos = todos.filter((t) => t !== todo))} />
+{/each}
---
+++ src/routes/Todo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Card, { Content } from '@smui/card';
+	import Actions from '@smui/card/src/Actions.svelte';
+	import Checkbox from '@smui/checkbox';
+	import FormField from '@smui/form-field';
+	import IconButton from '@smui/icon-button';
+	import { createEventDispatcher } from 'svelte';
+
+	export let todo: { text: string; completed: boolean };
+
+	const dispatch = createEventDispatcher<{ delete: void }>();
+</script>
+
+<Card padded>
+	<Content style="color: {todo.completed ? 'green' : 'unset'}">{todo.text}</Content>
+	<Actions>
+		<IconButton on:click={() => dispatch('delete')} class="material-icons">delete</IconButton>
+		<FormField>
+			<Checkbox bind:checked={todo.completed} />
+			<span slot="label">Completed</span>
+		</FormField>
+	</Actions>
+</Card>
